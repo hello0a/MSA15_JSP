e06ea9557f40828de4f8b26931a8158482136679
hello0a <aya70713@gmail.com>
2025-12-10 18:29:13 +0900

    jsp

--- Servlet/src/main/java/content/HtmlServlet.java
+++ Servlet/src/main/java/content/HtmlServlet.java
@@ -1,12 +1,13 @@
 package content;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet("/HtmlServlet")
 public class HtmlServlet extends HttpServlet {
--- Servlet/src/main/webapp/content/download.jsp
+++ Servlet/src/main/webapp/content/download.jsp
@@ -10,7 +10,11 @@
 	<h1>파일 다운로드</h1>
 	<form action="/Servlet/download" method="get">
 		<label for="fileName">파일명 : </label>
-		<input type="text" name="fileName" id="fileName">
+		<input type="text" name="fileName"
+		
+		
+		
+		 id="fileName">
 		<br>
 		<input type="submit" value="다운로드">
 	</form>
